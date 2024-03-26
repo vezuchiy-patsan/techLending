164f7c4bd7f2e05170a925db017297c0e059200d
Daniil Akhiyan <daniil.axiyan.316@mail.ru>
2024-03-26 17:11:22 +0300

    d

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
     mode: "development",
-    entry: path.resolve(__dirname, 'src'),
+    entry: path.resolve(__dirname, 'html/js/'),
     output: {
         path: path.resolve(__dirname, 'html'),
     },
