d70aeae2b7e4c64a20a468fff8a3124b5db155b6
Daniil Akhiyan <daniil.axiyan.316@mail.ru>
2024-04-12 10:44:47 +0300

    Сделал обработчик ошибок

--- html/php/sendTelegram.php
+++ html/php/sendTelegram.php
@@ -1,60 +1,56 @@
 <?php
-// $text = 'Новая заявка #1111';
-// $text .= PHP_EOL.'<b>Сайт</b>: <a href="tel:89052714903">адрес</a>';
-// $text .= PHP_EOL.'<b>Страница</b>: Название страницы - <a href="https://garvex.tech">Адрес страницы</a>';
-// $text .= PHP_EOL.'<b>Название формы</b>: Название формы';
-// $text .= PHP_EOL.'<b>Номер заявки</b>: id';
-// $text .= PHP_EOL.'<b>ФИО</b>: ФИО';
-// $text .= PHP_EOL.'<b>Телефон</b>: <a href="tel:89052714903">номер</a>';
-// $text .= PHP_EOL.'<b>Почта</b>: <a href="mailto:daniil.axiyan.316@mail.ru">почта</a>';
-// $text .= PHP_EOL.'<b>Сообщение</b>: Сообщение'; 
-
-enum MyEnum: int
-{
-    case gut_id = -4144784863;
-    case eng_id = -4172454446;
-    case tpk_id = -4166978429;
-    case test_dan = 180823165;
-}
-
-$botApiToken = '6938885385:AAGJlfJeG98ufjh91bvR4OcPdKtq059cl_4';
-$channelId = MyEnum::gut_id;
-$text = 'Новая заявка ГУТ';
-$text .= PHP_EOL.'<b>Сайт</b>: <a href="'.$_POST['link'].'">'.$_POST['title'].'</a>';
-$text .= PHP_EOL.'<b>Название формы</b>: '.$_POST['name_form'];
-$text .= PHP_EOL.'<b>ФИО</b>: '.$_POST['name'];
-$text .= PHP_EOL.'<b>Телефон</b>: '.$_POST['tel'];
-
-$query = http_build_query([
-    'chat_id' => $channelId->value,
-    'text' => $text,
-    'parse_mode' => 'HTML'
-]);
-$url = "https://api.telegram.org/bot{$botApiToken}/sendMessage?{$query}";
-
-$curl = curl_init();
-
-curl_setopt_array($curl, array(
-    CURLOPT_URL => $url,
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_CUSTOMREQUEST => 'POST',
-));
-
-$response = curl_exec($curl);
-
-if ($response === false) {
-    // Если curl_exec вернул false, это означает, что произошла ошибка
-    echo 'Curl error: ' . curl_error($curl);
-} else {
-    // Преобразование JSON-строки в ассоциативный массив
-    $responseArray = json_decode($response, true);
-
-    // Проверка на наличие ключа 'ok' в массиве
-    if ($responseArray['ok'] === true) {
-        echo 'Значение": ' . $responseArray['result'];
-    } else {
-        echo 'Ошибка: ' . $responseArray['error_code'];
-        echo 'Описание: ' . $responseArray['description'];
+try {
+    // PHP ^8.1
+    enum MyEnum: int
+    {
+        case gut_id = -4144784863;
     }
+    // токен
+    $botApiToken = '6938885385:AAGJlfJeG98ufjh91bvR4OcPdKtq059cl_4';
+
+    $channelId = MyEnum::gut_id;
+
+    // формирование параметров
+    $query = http_build_query([
+        'chat_id' => $channelId->value,
+        'text' => $text,
+        'parse_mode' => 'HTML'
+    ]);
+
+    $text = 'Новая заявка ГУТ';
+    $text .= PHP_EOL . '<b>Сайт</b>: <a href="' . $_POST['link'] . '">' . $_POST['title'] . '</a>';
+    $text .= PHP_EOL . '<b>Название формы</b>: ' . $_POST['name_form'];
+    $text .= PHP_EOL . '<b>ФИО</b>: ' . $_POST['name'];
+    $text .= PHP_EOL . '<b>Телефон</b>: ' . $_POST['tel'];
+
+    $url = "https://api.telegram.org/bot{$botApiToken}/sendMessage?{$query}";
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+    ));
+
+    $response = curl_exec($curl);
+
+    if ($response === false) {
+        // Если curl_exec вернул false, это означает, что произошла ошибка
+        echo 'Curl error: ' . curl_error($curl);
+    } else {
+        // Преобразование JSON-строки в ассоциативный массив
+        $responseArray = json_decode($response, true);
+
+        // Проверка на наличие ключа 'ok' в массиве
+        if ($responseArray['ok'] === true) {
+            echo 'Значение": ' . $responseArray['result'];
+        } else {
+            echo 'Ошибка: ' . $responseArray['error_code'];
+            echo 'Описание: ' . $responseArray['description'];
+        }
+    }
+    curl_close($curl);
+} catch (\Throwable $th) {
+    echo "Ошибка при выполнении программы" . $th->getMessage();
 }
-curl_close($curl);
--- html/php/sendmessage.php
+++ html/php/sendmessage.php
@@ -1,4 +1,5 @@
 <?php
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -7,49 +8,52 @@ require_once '../php/phpmailer/phpmailer.php';
 require_once '../php/phpmailer/smtp.php';
 require_once '../php/sendTelegram.php';
 
+try {
+	$mail = new PHPMailer;
+	$mail->CharSet = 'UTF-8';
 
-$mail = new PHPMailer;
-$mail->CharSet = 'UTF-8';
+	// Настройки SMTP
+	$mail->isSMTP();
+	$mail->SMTPAuth = true;
+	$mail->SMTPDebug = 0;
+	$mail->Host = 'ssl://smtp.mail.ru';
+	$mail->Port = 465;
+	$mail->Username = 'info@garveks.ru';
+	$mail->Password = 'jp8k3kyTFrtcc2SirTiN';
 
-// Настройки SMTP
-$mail->isSMTP();
-$mail->SMTPAuth = true;
-$mail->SMTPDebug = 0;
-$mail->Host = 'ssl://smtp.mail.ru';
-$mail->Port = 465;
-$mail->Username = 'info@garveks.ru';
-$mail->Password = 'jp8k3kyTFrtcc2SirTiN';
+	// От кого
+	$mail->setFrom('info@garveks.ru', 'Info');
 
-// От кого
-$mail->setFrom('info@garveks.ru', 'Info');
+	// Кому
+	$mail->addAddress('smart@garvex.tech', 'Smart');
+	// $mail->addAddress('sale@garveks.ru', 'Sale');
 
-// Кому
-$mail->addAddress('smart@garvex.tech', 'Smart');
-// $mail->addAddress('sale@garveks.ru', 'Sale');
+	// Тема письма
+	$mail->Subject = 'Новая заявка c garvex.tech';
 
-// Тема письма
-$mail->Subject = 'Новая заявка c garvex.tech';
+	//Тело письма
+	$userphone = $_POST['tel'];
+	$username = $_POST['name'];
+	// isset($_POST['email']) ? $useremail = nl2br($_POST['email']) : $usertext = nl2br($_POST['message']);
+	$usertitle = $_POST['title'];
 
-//Тело письма
-$userphone = $_POST['tel'];
-$username = $_POST['name'];
-// isset($_POST['email']) ? $useremail = nl2br($_POST['email']) : $usertext = nl2br($_POST['message']);
-$usertitle = $_POST['title'];
+	$msg  = "<html><body style='font-family:Arial,sans-serif;'>";
+	$msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Новая заявка</h2>\r\n";
+	$msg .= "<p><strong>Имя:</strong> " . $username . "</p>\r\n";
+	$msg .= "<p><strong>Телефон:</strong> " . $userphone . "</p>\r\n";
+	$msg .= "</body></html>";
 
-$msg  = "<html><body style='font-family:Arial,sans-serif;'>";
-$msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Новая заявка</h2>\r\n";
-$msg .= "<p><strong>Имя:</strong> " . $username . "</p>\r\n";
-$msg .= "<p><strong>Телефон:</strong> " . $userphone . "</p>\r\n";
-$msg .= "</body></html>";
+	$body = $msg;
+	$mail->msgHTML($body);
 
-$body = $msg;
-$mail->msgHTML($body);
+	if ($mail->send()) {
+		echo 'Message has been sent';
+	} else {
 
-if ($mail->send()) {
-	echo 'Message has been sent';
-} else {
+		echo 'Message could not be sent.';
 
-	echo 'Message could not be sent.';
-
-	echo 'Mailer Error: ' . $mail->ErrorInfo;
+		echo 'Mailer Error: ' . $mail->ErrorInfo;
+	}
+} catch (\Throwable $th) {
+	echo "Ошибка при выполнении программы" . $th->getMessage();
 }
